29b73beb286eb04b1ef7ca7e33fd4519dbbd6055
Erica L. Ingram <inquiries@aquoco.co>
2020-05-25 20:19:15 -0700

    rules component done

--- app/cs-bw/src/components/Rules.js
+++ app/cs-bw/src/components/Rules.js
@@ -2,11 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import { Route, Switch, Link } from 'react-router-dom';
 
+
+
 function Footer() {
 	return (
 		<div>
 			<header>
-				<p>RULES COMPONENT</p>
+				<h1>Rules:</h1>
+				<ul>
+					<li>Any live cell with two or three live neighbors survive; any others die.</li>
+					<li>Any dead cell with three live neighbors becomes alive; else remains dead.</li>
+					<li>All other live cells in next generation die and all dead remain dead.</li>
+				</ul>
 			</header>
 		</div>
 	);
