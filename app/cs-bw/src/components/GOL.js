ceec80bdbc6afe61fd6c1bc2ffc1984272aeb09f
Erica L. Ingram <inquiries@aquoco.co>
2020-05-26 10:02:55 -0700

    colored one cell; separated out code into different functions

--- app/cs-bw/src/components/GOL.js
+++ app/cs-bw/src/components/GOL.js
@@ -10,82 +10,93 @@ const Canvas = styled.canvas`
 `
 
 class GOL extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            running: false,
-            canvasGrid: [],
-            cWidth: 0,
-            cHeight: 0,
-            currentGeneration: 0
-        };
-    }
-    
-    // Colors:  #2958AA (blue), #4E8A63 (green), #642B73 (purple), #C6426E (pink)
+	constructor(props) {
+		super(props);
+		this.state = {
+			running: false,
+			canvasGrid: [],
+			cellLifeGrid: [],
+			cWidth: 0,
+			cHeight: 0,
+			currentGeneration: 0
+		};
+	}
 
-    componentDidMount() {
-        let canvasGrid = new Array(255).fill(0);
-        let actualCanvas = this.refs.canvas;
-        let cContext = actualCanvas.getContext('2d');
-        let cWidth = cContext.canvas.width;
-        let cHeight = cContext.canvas.height;
-        let boxWidth = cWidth / 25;
-        let boxHeight = cHeight / 25;
+	// Colors:  #2958AA (blue), #4E8A63 (green), #642B73 (purple), #C6426E (pink)
 
-        for (let x = 0; x <= cWidth; x += boxWidth) {
+    setLive = (cContext, x, y, boxWidth, boxHeight) => {
+        // TODO:  Add live state
+		cContext.fillStyle = `rgb(78, 138, 99)`;
+		cContext.fillRect(x, y, boxWidth, boxHeight);
+	};
+
+	setDead = (cContext, x, y, boxWidth, boxHeight) => {
+        // TODO:  Add dead state
+		cContext.fillStyle = `rgb(41, 88, 170)`;
+		cContext.fillRect(x, y, boxWidth, boxHeight);
+	};
+
+    defaultFill = (cContext, cWidth, boxWidth, cHeight, boxHeight) => { 
+		for (let x = 0; x <= cWidth; x += boxWidth) {
 			for (let y = 0; y <= cHeight; y += boxHeight) {
 				cContext.moveTo(x, 0);
 				cContext.lineTo(x, cHeight);
 				cContext.stroke();
 				cContext.moveTo(0, y);
 				cContext.lineTo(cWidth, y);
-                cContext.stroke();
-                // if cell is alive/dead 
-                if (x === 720 & y === 390) { 
-                    cContext.fillStyle = `rgb(78, 138, 99)`;
-                    cContext.fillRect(x, y, boxWidth, boxHeight);
-
-                }
-                else{
-                    cContext.fillStyle = `rgb(41, 88, 170)`;
-                    cContext.fillRect(x, y, boxWidth, boxHeight);
-                }
+				cContext.stroke();
+				// TODO:  if cell is alive/dead
+				if ((x === 720) & (y === 390)) {
+					this.setLive(cContext, x, y, boxWidth, boxHeight);
+				} else {
+					this.setDead(cContext, x, y, boxWidth, boxHeight);
+				}
 			}
-        }
-        
-        let imageData = cContext.getImageData(0, 0, cWidth, cHeight);
+		}
+    };
+    
+	componentDidMount() {
+		let canvasGrid = new Array(255).fill(0);
+		let actualCanvas = this.refs.canvas;
+		let cContext = actualCanvas.getContext('2d');
+		let cWidth = cContext.canvas.width;
+		let cHeight = cContext.canvas.height;
+		let boxWidth = cWidth / 25;
+		let boxHeight = cHeight / 25;
 
-        let screenBuffer = imageData.data;
-                
-        let x = 10,
-            y = 20;
-        
-        let index = (y * cWidth + x) * 4;
+		this.defaultFill(cContext, cWidth, boxWidth, cHeight, boxHeight);
 
-        screenBuffer[index + 0] = 0x4e; // Red
-        screenBuffer[index + 1] = 0x8a; // Green
-        screenBuffer[index + 2] = 0x63; // Blue
-        screenBuffer[index + 3] = 0xff; // Alpha
+		/*
+		let imageData = cContext.getImageData(0, 0, cWidth, cHeight);
 
-        cContext.putImageData(imageData, 0, 0);
+		let screenBuffer = imageData.data;
 
-        /*
+		let x = 10,
+			y = 20;
+
+		let index = (y * cWidth + x) * 4;
+
+		screenBuffer[index + 0] = 0x4e; // Red
+		screenBuffer[index + 1] = 0x8a; // Green
+		screenBuffer[index + 2] = 0x63; // Blue
+		screenBuffer[index + 3] = 0xff; // Alpha
+
+		cContext.putImageData(imageData, 0, 0);
+
             this.setState({
                 canvasGrid: canvasGrid,
                 cWidth: boxWidth,
                 cHeight: boxHeight
             });
         */
-    }
-
-    
-    render() {
-        return (
-            <div>
-                <Canvas id="canvas" ref="canvas" width="750" height="650"></Canvas>
-            </div>
-        );
-    }
+	}
+	render() {
+		return (
+			<div>
+				<Canvas id="canvas" ref="canvas" width="750" height="650"></Canvas>
+			</div>
+		);
+	}
 }
 
 export default GOL;
